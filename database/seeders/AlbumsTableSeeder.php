0113b4bbda047c63b4211f01049050ec6b5f5791
berk <ps239891@summacollege.nl>
2023-11-22 15:10:12 +0100

    seeder

--- database/seeders/AlbumsTableSeeder.php
+++ database/seeders/AlbumsTableSeeder.php
@@ -2,17 +2,54 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class AlbumsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
     public function run()
-{
-    factory(App\Album::class, 5)->create();
-}
+    {
+        DB::table('albums')->insert([
+            [
+                'name' => 'Album 1',
+                'year' => 2005,
+                'times_sold' => 100000,
+            ],
+            [
+                'name' => 'Album 2',
+                'year' => 2012,
+                'times_sold' => 75000,
+            ],
+            [
+                'name' => 'Album 3',
+                'year' => 2018,
+                'times_sold' => 50000,
+            ],
+            [
+                'name' => 'Album 4',
+                'year' => 2008,
+                'times_sold' => 120000,
+            ],
+            [
+                'name' => 'Album 5',
+                'year' => 2015,
+                'times_sold' => 90000,
+            ],
+        ]);
 
+        // // foreach ($albums as $album) {
+        //     DB::table('albums')->insert([
+        //         'name' => $album['name'],
+        //         'year' => $album['year'],
+        //         'times_sold' => $album['times_sold'],
+        //         'created_at' => now(),
+        //         'updated_at' => now(),
+        //     ]);
+        // // }
+    }
 }
--- database/seeders/BandsTableSeeder.php
+++ database/seeders/BandsTableSeeder.php
@@ -2,17 +2,48 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class BandsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
     public function run()
     {
-        factory(App\Band::class, 3)->create();
+        $bands = [
+            [
+                'name' => 'Band 1',
+                'genre' => 'Rock',
+                'founded' => 2000,
+                'active_till' => 'Heden',
+            ],
+            [
+                'name' => 'Band 2',
+                'genre' => 'Pop',
+                'founded' => 1995,
+                'active_till' => 'Heden',
+            ],
+            [
+                'name' => 'Band 3',
+                'genre' => 'Metal',
+                'founded' => 2010,
+                'active_till' => '2022',
+            ],
+        ];
+
+        foreach ($bands as $band) {
+            DB::table('bands')->insert([
+                'name' => $band['name'],
+                'genre' => $band['genre'],
+                'founded' => $band['founded'],
+                'active_till' => $band['active_till'],
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
-    
 }
