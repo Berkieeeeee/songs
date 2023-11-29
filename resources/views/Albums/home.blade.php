0a49726e183d4d096b89869255897952ae369f01
berk <ps239891@summacollege.nl>
2023-11-29 13:27:54 +0100

    d

--- resources/views/Albums/home.blade.php
+++ resources/views/Albums/home.blade.php
@@ -1,16 +1,1 @@
-<!-- resources/views/albums/index.blade.php -->
 
-@extends('layouts.app')
-
-@section('content')
-
-<h1>All Albums</h1>
-
-@foreach ($albums as $album)
-    <div>
-        <h3>{{ $album->name }}</h3>
-        {{-- Add other album details as needed --}}
-    </div>
-@endforeach
-
-@endsection
--- resources/views/Bands/index.blade.php
+++ resources/views/Bands/index.blade.php
@@ -2,13 +2,21 @@
 
 @section('content')
 
-<h1>All Bands</h1>
-
-@foreach ($bands as $band)
-    <div>
-        <h3>{{ $band->name }}</h3>
-        {{-- Add other band details as needed --}}
-    </div>
-@endforeach
+<div class="container">
+    <h1>All Bands</h1>
+    @forelse ($bands as $band)
+        <div class="card mb-3">
+            <div class="card-body">
+                <h3 class="card-title">{{ $band->name }}</h3>
+                <p class="card-text"><strong>Genre:</strong> {{ $band->genre }}</p>
+                <p class="card-text"><strong>Formed In:</strong> {{ $band->formed_in }}</p>
+                {{-- Add other band details as needed --}}
+                <a href="{{ route('bands.show', $band->id) }}" class="btn btn-primary">View Details</a>
+            </div>
+        </div>
+    @empty
+        <p>No bands available.</p>
+    @endforelse
+</div>
 
 @endsection
